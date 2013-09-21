0f7b09d249e2f637d4ad096f42b42fbddc4f4611
EvgenyKarkan <evgenykarkan@gmail.com>
2013-09-21 20:12:40 +0300

    Added plist provider.

---
+++ iSayNo/iSayNo/Classes/Helpers/EKPlistDataProvider.m
@@ -0,0 +1,13 @@
+//
+//  EKPlistDataProvider.m
+//  iKnowE
+//
+//  Created by Evgeny Karkan on 21.09.13.
+//  Copyright (c) 2013 EvgenyKarkan. All rights reserved.
+//
+
+#import "EKPlistDataProvider.h"
+
+@implementation EKPlistDataProvider
+
+@end

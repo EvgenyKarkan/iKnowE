bf13a5cf446ec596c8ceb577e2b80ecb6fc5a5ab
EvgenyKarkan <evgenykarkan@gmail.com>
2013-09-28 22:53:11 +0300

    Keyboard handling.

--- iKnowE/iKnowE/Classes/Controllers/EKAddEViewController.m
+++ iKnowE/iKnowE/Classes/Controllers/EKAddEViewController.m
@@ -46,55 +46,38 @@
 
 - (void)registerForKeyboardNotifications
 {
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardWasShown:)
-                                                 name:UIKeyboardDidShowNotification object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+	                                         selector:@selector(keyboardWasShown:)
+	                                             name:UIKeyboardDidShowNotification object:nil];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardWillBeHidden:)
-                                                 name:UIKeyboardWillHideNotification object:nil];
-    
+	[[NSNotificationCenter defaultCenter] addObserver:self
+	                                         selector:@selector(keyboardWillBeHidden:)
+	                                             name:UIKeyboardWillHideNotification object:nil];
 }
 
 - (void)keyboardWasShown:(NSNotification *)aNotification
 {
-    CGRect keyboardFrame = [[[aNotification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
-    CGRect convertedKeyBoardFrame = [self.view convertRect:keyboardFrame fromView:self.view.window];
-    CGRect convertedScrollFrame = [self.view convertRect:self.addView.scrollView.frame toView:self.view.window];
+	CGRect keyboardFrame = [[[aNotification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+	CGRect convertedKeyBoardFrame = [self.view convertRect:keyboardFrame fromView:self.view.window];
     
-
+	CGFloat margin;
     
-    CGSize kbSize = convertedKeyBoardFrame.size;
+	if ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait) {
+		margin = 20.0f;
+	}
+	else {
+		margin = 235.0f;
+	}
     
-    
-    NSLog(@"Scroll: %@", NSStringFromCGRect(self.addView.scrollView.frame));
-    NSLog(@"Scroll converted: %@", NSStringFromCGRect(convertedScrollFrame));
-    
-    NSLog(@"My view add: %@", NSStringFromCGRect(self.addView.information.frame));
-    NSLog(@"KeyBoard converted: %@", NSStringFromCGRect(convertedKeyBoardFrame));
-    
-    if (CGRectIntersectsRect(convertedScrollFrame, convertedKeyBoardFrame)) {
-
-        NSLog(@"FRAMES INTERSECTS");
-    }
-    
-    NSLog(@"S Y %f", convertedScrollFrame.origin.y);
-    NSLog(@"K Y %f", convertedKeyBoardFrame.origin.y);
-    NSLog(@"Intersect %f",convertedKeyBoardFrame.origin.y - (self.addView.information.frame.origin.y + self.addView.information.frame.size.height) - 20.0f);
-    
-    
-    
-    [self.addView.information setFrame:CGRectMake(20.0f, self.addView.name.frame.origin.y + self.addView.name.frame.size.height + 20.0f,
-                                                  self.addView.scrollView.frame.size.width - 40.0f, convertedKeyBoardFrame.origin.y - (self.addView.information.frame.origin.y + self.addView.information.frame.size.height)/*kbSize.height*/)];
-    
+	[self.addView.information setFrame:CGRectMake(20.0f, self.addView.name.frame.origin.y + self.addView.name.frame.size.height + 20.0f,
+	                                              self.addView.scrollView.frame.size.width - 40.0f, self.addView.frame.size.height - (convertedKeyBoardFrame.origin.y - (self.addView.information.frame.origin.y + self.addView.information.frame.size.height)) - margin)];
 }
 
-- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+- (void)keyboardWillBeHidden:(NSNotification *)aNotification
 {
-    self.addView.information.frame = CGRectMake(20.0f, self.addView.name.frame.origin.y + self.addView.name.frame.size.height + 20.0f,
-                                                self.addView.scrollView.frame.size.width - 40.0f,
-                                                self.addView.scrollView.frame.size.height - 20.0f - self.addView.eCode.frame.size.height - self.addView.name.frame.size.height - 60.0f);
-
+	self.addView.information.frame = CGRectMake(20.0f, self.addView.name.frame.origin.y + self.addView.name.frame.size.height + 20.0f,
+	                                            self.addView.scrollView.frame.size.width - 40.0f,
+	                                            self.addView.scrollView.frame.size.height - 20.0f - self.addView.eCode.frame.size.height - self.addView.name.frame.size.height - 60.0f);
 }
 
 @end

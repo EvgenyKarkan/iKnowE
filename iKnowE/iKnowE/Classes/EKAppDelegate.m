4a720a6bbb1e28cce8bb50421d29ee63a9b6f124
EvgenyKarkan <evgenykarkan@gmail.com>
2013-10-14 18:54:30 +0300

    Submitted version 1.0.0

--- iKnowE/iKnowE/Classes/EKAppDelegate.m
+++ iKnowE/iKnowE/Classes/EKAppDelegate.m
@@ -20,28 +20,29 @@ static NSString * const kEKFlurryKey = @"QG97GTD5QD7JK6KJCCS2";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+	[NSThread sleepForTimeInterval:3.0f];
 	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-	
+    
 	EKListViewController *listViewController = [[EKListViewController alloc] init];
 	EKDetailViewController *detailViewController = [[EKDetailViewController alloc] init];
-	
+    
 	self.splitViewController = [[UISplitViewController alloc] init];
 	self.splitViewController.viewControllers = @[listViewController, detailViewController];
-    self.splitViewController.delegate = detailViewController;
+	self.splitViewController.delegate = detailViewController;
     
 	self.window.rootViewController = self.splitViewController;
 	[self.window makeKeyAndVisible];
-	
-    [self preloadSettingsInfo];
     
-        //[Flurry startSession:kEKFlurryKey];
+	[self preloadSettingsInfo];
     
-    return YES;
+	[Flurry startSession:kEKFlurryKey];
+    
+	return YES;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    [[EKCoreDataProvider sharedInstance] saveContext];
+	[[EKCoreDataProvider sharedInstance] saveContext];
 }
 
 - (void)preloadSettingsInfo
